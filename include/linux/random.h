769ab57fa5651026719c0f248937ec951e96ed88
Richard Henderson <richard.henderson@linaro.org>
2020-01-10 14:54:16 +0000

    linux/random.h: Remove arch_has_random, arch_has_random_seed
    commit 647f50d5d9d933b644b29c54f13ac52af1b1774d upstream.
    
    The arm64 version of archrandom.h will need to be able to test for
    support and read the random number without preemption, so a separate
    query predicate is not practical.
    
    Since this part of the generic interface is unused, remove it.
    
    Signed-off-by: Richard Henderson <rth@twiddle.net>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Link: https://lore.kernel.org/r/20200110145422.49141-5-broonie@kernel.org
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/random.h
+++ include/linux/random.h
@@ -133,6 +133,13 @@ static inline bool __must_check arch_get_random_seed_long(unsigned long *v)
 static inline bool __must_check arch_get_random_seed_int(unsigned int *v)
 {
 	return false;
+static inline bool arch_get_random_seed_long(unsigned long *v)
+{
+	return 0;
+}
+static inline bool arch_get_random_seed_int(unsigned int *v)
+{
+	return 0;
 }
 #endif
 
