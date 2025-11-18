974f152c79f0fa6ceb66b76f8b665acd34208a53
Pranav Vashi <neobuddy89@gmail.com>
2025-11-18 17:50:38 +0530

    fs: kernelsu: Add scope-minimized manual hooks 1.4
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/open.c
+++ fs/open.c
@@ -355,6 +355,12 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
  * switching the fsuid/fsgid around to the real ones.
  */
 long do_faccessat(int dfd, const char __user *filename, int mode)
+
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK)
+extern __attribute__((hot, always_inline)) int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
+			                    int *flags);
+#endif
+
 {
 	const struct cred *old_cred;
 	struct cred *override_cred;
@@ -363,6 +369,10 @@ long do_faccessat(int dfd, const char __user *filename, int mode)
 	struct vfsmount *mnt;
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
+
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK)
+	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
+#endif
 
 	if (mode & ~S_IRWXO)	/* where's F_OK, X_OK, W_OK, R_OK? */
 		return -EINVAL;
